46b35d6254eb816a283c473960b4acc7dfb9fb7f
wim-web <rootuser.main+github@gmail.com>
2019-08-20 03:02:02 +0900

    add: icon fonts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Books</title>
     <script src="{{ mix('js/app.js') }}" defer></script>
-
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@3.x/css/materialdesignicons.min.css" rel="stylesheet">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
